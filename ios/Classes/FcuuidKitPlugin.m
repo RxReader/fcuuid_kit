5af95e9b83b26b0b73010edfbbac3e4d54e0385d
v7lin <v7lin@qq.com>
2021-05-11 14:05:31 +0800

    初始化

---
+++ ios/Classes/FcuuidKitPlugin.m
@@ -0,0 +1,20 @@
+#import "FcuuidKitPlugin.h"
+
+@implementation FcuuidKitPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fcuuid_kit"
+            binaryMessenger:[registrar messenger]];
+  FcuuidKitPlugin* instance = [[FcuuidKitPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
